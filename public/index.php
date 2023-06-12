ea4de7fae17dcf91cd331dd0cf8ac24dfcb54447
tom daret <daret.tom@gmail.com>
2023-06-12 14:13:28 +0200

    feat: configuration d'index.php en attente des images

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
 declare(strict_types=1);
+
+
+use Entity\Collection\MovieCollection;
+use Html\AppWebPage;
+
+$webPage = new AppWebPage('films');
+
+$webPage->appendContent('<main class="list">');
+foreach(MovieCollection::findAll() as $movie) {
+    $webPage->appendContent(
+        <<<HTML
+<a href="movie.php?movieId={$movie->getId()}">
+    <span>{$webPage->escapeString($movie->getTitle())}</span>
+</a>
+
+HTML
+    );
+}
+$webPage->appendContent('</main>');
+echo $webPage->toHTML();
