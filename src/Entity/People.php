eb3f90856395a547b05f7197341c64b2f3310352
Mathis Claverie <mathis.claverie@etudiant.univ-reims.fr>
2023-06-12 14:23:53 +0200

    Création de la classe People

---
+++ src/Entity/People.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+namespace Entity;
+
+class People
+{
+private int $id;
+private int $avatarid;
+private string $birthday;
+private ?string $deathday;
+private string $name;
+private string $biography;
+private string $placeOfBirth;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAvatarid(): int
+    {
+        return $this->avatarid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDeathday(): ?string
+    {
+        return $this->deathday;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlaceOfBirth(): string
+    {
+        return $this->placeOfBirth;
+    }
+
+}
