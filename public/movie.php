95946b91ae8a9096277d09f4cf2992228a1d1d8e
Mathis Claverie <mathis.claverie@etudiant.univ-reims.fr>
2023-06-12 15:04:31 +0200

    update movie.php , script incomplet

--- public/movie.php
+++ public/movie.php
@@ -21,3 +21,21 @@ try {
     http_response_code(404);
     exit();
 }
+$peoples= $movie->getPeople();
+$webPage->setTitle("Titre -".$movie->getTitle());
+$webPage->appendContent(<<<HTML
+<div class="movie__info">
+    <img src="image.php?movieId={$movie->getId()}">
+    <div class="movie__text">
+        <div class="title__date">
+            <div class="movie__title">{$movie->getTitle()}</div>
+            <div class="movie__date">{$movie->getReleaseDate()}</div>
+            
+        </div>
+        <div class="movie__originalTitle">{$movie->getOriginalTitle()}</div>
+        <div class="movie__TagLine">{$movie->getTagline()}</div>    
+        <div class="movie__Overview">{$movie->getOverview()}</div>
+    </div>
+</div>
+HTML
+);
