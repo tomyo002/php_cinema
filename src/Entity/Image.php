3b2582aec2a48f2836c896375bb0d4e9cab0857a
Mathis Claverie <mathis.claverie@etudiant.univ-reims.fr>
2023-06-12 13:44:48 +0200

    création de la classe Image , ajout des getters et de la methode finById

---
+++ src/Entity/Image.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+namespace Entity;
+
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
+class Image
+{
+    private int $id;
+    private string $jpeg;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+    public static function findById(int $id): Image
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+    SELECT *
+    FROM image
+    WHERE id = :id
+SQL
+        );
+        $stmt->execute([':id' => $id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Image::class);
+        if (($cover = $stmt->fetch()) === false) {
+            throw new EntityNotFoundException("Pas de cover pour l'id {$id}");
+        }
+        return $cover;
+    }
+}
