f98eecdb53b3e18702bf2314ef99806453db65f1
tom daret <daret.tom@gmail.com>
2023-06-12 11:11:42 +0200

    création des ascenseurs de la classe Movie

--- src/entity/Movie.php
+++ src/entity/Movie.php
@@ -14,4 +14,78 @@ class Movie
     private int $runtime;
     private string $tagline;
     private string $title;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalLanguage(): string
+    {
+        return $this->originalLanguage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalTitle(): string
+    {
+        return $this->originalTitle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReleaseDate(): string
+    {
+        return $this->releaseDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRuntime(): int
+    {
+        return $this->runtime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTagline(): string
+    {
+        return $this->tagline;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+
 }
