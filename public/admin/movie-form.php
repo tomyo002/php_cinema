058e2a5fb5c7d1ff9bd941103cf3d0b9aa964034
tom daret <daret.tom@gmail.com>
2023-06-13 15:07:09 +0200

    creation des scripts

---
+++ public/admin/movie-form.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Entity\Exception\EntityNotFoundException;
+use Entity\Exception\ParameterException;
+use Entity\Movie;
+use Html\AppWebPage;
+use Html\Form\MovieForm;
+
+try {
+    $appWebPage= new AppWebPage("Information Film");
+    $movie= null;
+    if(isset($_GET['movieId'])) {
+        if(!ctype_digit($_GET['movieId'])) {
+            throw new ParameterException();
+        }
+        $movie = Movie::findById((int)$_GET['movieId']);
+    }
+    $movieForm = new movieForm($movie);
+    $appWebPage->appendContent($movieForm->getHtmlForm("movie-save.php"));
+    echo $appWebPage->toHTML();
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
