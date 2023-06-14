04f64610663fae7eec4f6627641699e0420633fb
tom daret <daret.tom@gmail.com>
2023-06-14 15:16:36 +0200

    documentation

--- src/Entity/transformFrenchDate.php
+++ src/Entity/transformFrenchDate.php
@@ -5,6 +5,13 @@ namespace Entity;
 
 trait transformFrenchDate
 {
+    /**
+     * Transforme une date en française prise en paramètre
+     * retourne une valeur sous forme de chaîne de caractère
+     *
+     * @param string $date
+     * @return string
+     */
     public function transformFrenchDate(string $date):string
     {
         return implode('/', array_reverse(explode('-', $date)));
