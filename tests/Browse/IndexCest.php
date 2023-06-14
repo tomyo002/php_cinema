d20650610a259c97f2c2c9abfab7e90d23f13fd1
tom daret <daret.tom@gmail.com>
2023-06-14 18:05:46 +0200

    validation test

---
+++ tests/Browse/IndexCest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Browse;
+
+use Tests\BrowseTester;
+
+class IndexCest
+{
+    public function checkAppWebPageHtmlStructure(BrowseTester $I):void
+    {
+        $I->amOnPage('/');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('films');
+        $I->seeElement('.header');
+        $I->seeElement('.header h1');
+        $I->see('films', '.header h1');
+        $I->seeElement('.content');
+        $I->seeElement('.footer');
+    }
+
+    public function clickOnArtistLink(BrowseTester $I):void
+    {
+        $I->amOnPage('/');
+        $I->seeResponseCodeIs(200);
+        $I->click('Trois couleurs : Rouge');
+        $I->seeInCurrentUrl('/movie.php?movieId=110');
+    }
+}
